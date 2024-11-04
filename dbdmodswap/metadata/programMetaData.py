ba91b7acc4ab354132f95364f04eca30700ed287
Ross Adamson <leopard.ross@gmail.com>
2024-11-04 11:25:02 -0600

    Bump version to 0.3.1 - bugfix release

--- dbdmodswap/metadata/programMetaData.py
+++ dbdmodswap/metadata/programMetaData.py
@@ -1,6 +1,6 @@
 ProgramName = 'DbdModSwap'
 ProgramFilename = f'{ProgramName}.exe'
-Version = '0.3.0'
+Version = '0.3.1'
 Author = 'Ross Adamson'
 License = 'MIT'
 ConsoleTitle = f'{ProgramName} - By Ross'
