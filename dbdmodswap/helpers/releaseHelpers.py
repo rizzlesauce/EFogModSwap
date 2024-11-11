93fd35f12e04c35ad905b202d032b2ef82c792b7
Ross Adamson <leopard.ross@gmail.com>
2024-11-11 09:02:57 -0600

    fixup! Notify new release version link

---
+++ dbdmodswap/helpers/releaseHelpers.py
@@ -0,0 +1,27 @@
+import requests
+
+from dbdmodswap.metadata.programMetaData import GithubProjectOwnerName, GithubProjectRepoName
+
+
+def getGithubProjectUrl():
+    return f'https://github.com/{GithubProjectOwnerName}/{GithubProjectRepoName}'
+
+
+def getGithubApiProjectUrl():
+    return f'https://api.github.com/repos/{GithubProjectOwnerName}/{GithubProjectRepoName}'
+
+
+def getGithubProjectReleasesUrl():
+    return f'{getGithubProjectUrl()}/releases'
+
+
+def getGithubProjectReleaseUrl(version=None):
+    return f"{getGithubProjectReleasesUrl()}/{f'tags/v{version}' if version else 'latest'}"
+
+
+def getLatestReleaseVersion():
+    try:
+        response = requests.get(f"{getGithubApiProjectUrl()}/releases/latest")
+        return response.json()['name'].removeprefix('v')
+    except:
+        pass
