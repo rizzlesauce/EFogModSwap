4673f4a08e4e6d3a5ff9fdfd180ca69806001418
Ross Adamson <leopard.ross@gmail.com>
2025-03-11 08:01:55 -0500

    Bump version to 0.3.10

--- modswap/metadata/programMetaData.py
+++ modswap/metadata/programMetaData.py
@@ -1,6 +1,6 @@
 ProgramName = 'EFogModSwap'
 ProgramFilename = f'{ProgramName}.exe'
-Version = '0.3.9'
+Version = '0.3.10'
 Author = 'Ross Adamson'
 License = 'MIT'
 ConsoleTitle = f'{ProgramName} - By Ross'
